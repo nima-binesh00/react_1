adb35950682e20aec3111bd7b096f8a0bdc58c94
nima-binesh00 <nimabinesh00@gmail.com>
2025-05-19 16:16:03 +0330

    add_homework

---
+++ New folder/nima_homework/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+import HelloWorld from "./HelloWorld.jsx";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
